29037c7141d5a9ea815452e998a2df3c34de46a9
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-08-25 15:42:38 +0100

    first commit

---
+++ Downloads/SendMailWebApi-master/SendmailAPI/services/EmailServices.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using SendmailAPI.Interfaces;
+using SendmailAPI.Models;
+
+namespace SendmailAPI.services
+{
+    public class EmailServices : IMailServices
+    {
+        private readonly Appsetting _appsetting;
+
+        public EmailServices(IServiceProvider provider)
+        {
+            _appsetting = provider.GetRequiredService<Appsetting>();
+        }
+        public void SendEmail(EmailReceiver emailReceiver)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_appsetting.EmailUserName));
+            email.To.Add(MailboxAddress.Parse(emailReceiver.To));
+            email.Subject = emailReceiver.Subject;
+            email.Body = new TextPart(TextFormat.Plain) { Text = emailReceiver.Body};
+            using var smtp = new SmtpClient();
+            smtp.Connect(_appsetting.EmailHost, _appsetting.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_appsetting.EmailUserName, _appsetting.EmailPassword);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
